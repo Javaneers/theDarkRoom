da83be0713b4aa34b2e5fbea1f43f4f7906aa105
ZenMasterOwl <michaelvance0416@gmail.com>
2020-06-23 08:46:10 -0700

    changed PlayerInput name to ReadPlayerInput

--- com.javaneers/src/com/javaneers/game/PlayerInput.java
+++
@@ -1,9 +0,0 @@
-package com.javaneers.game;
-
-import java.util.Scanner;
-
-public class PlayerInput {
-  private int input = 2;
-
-  public int getInput(){ return input; }
-}
---
+++ com.javaneers/src/com/javaneers/game/ReadPlayerInput.java
@@ -0,0 +1,11 @@
+package com.javaneers.game;
+
+import java.util.Scanner;
+
+public class ReadPlayerInput {
+  private static String input = "2";
+
+  public static String getInput(){ return input; }
+
+
+}
