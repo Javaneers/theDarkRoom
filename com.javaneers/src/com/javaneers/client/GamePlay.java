145061d71d5d138271fa6fe85dec4bfb0de4be52
ZenMasterOwl <michaelvance0416@gmail.com>
2020-06-23 12:14:14 -0700

    Started sequencing the order of events as method calls in GamePlay main method

--- com.javaneers/src/com/javaneers/client/GamePlay.java
+++ com.javaneers/src/com/javaneers/client/GamePlay.java
@@ -1,9 +1,22 @@
 package com.javaneers.client;
 
+import com.javaneers.game.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // this will contain the loading, turn, and credit method calls
 public class GamePlay {
 
   public static void main(String[] args) {
+
+    Screens titleScreen = new Screens("Dark Room","",new ArrayList<String>(List.of("Rules","Start")));
+    titleScreen.drawHeader();
+    GameBoard board1 = new GameBoard(5,5);
+    //GameBoard might initialize player eventually, doing that manually in the client for now
+    PlayerSprite player1 = new PlayerSprite();
+    player1.movePayer();
+
     System.out.println("placeholder text");
   }
 
