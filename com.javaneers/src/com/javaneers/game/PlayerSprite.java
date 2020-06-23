f6b9e1e82cbde2b58550ee1402529812317bc2df
mayamarks <maya.marks.dc@gmail.com>
2020-06-23 09:58:26 -0700

    the lost changes added again

--- com.javaneers/src/com/javaneers/game/PlayerSprite.java
+++ com.javaneers/src/com/javaneers/game/PlayerSprite.java
@@ -5,42 +5,43 @@ package com.javaneers.game;
 public class PlayerSprite {
 
   private int moveCount = 5; //hardcoded here for now
+  GameBoard board = new GameBoard();
 
   //Business Methods
   public void movePayer() throws IllegalArgumentException {
-    int currentRow = GameBoard.getPlayerPosition( int a);
-    int currentCol = GameBoard.getPlayerPosition( int b);
-    String input = PlayerInteraction.getInput();
+    int currentRow = 3; //board.getPlayerPosition("row");
+    int currentCol = 4; //board.getPlayerPosition("col");
+    int input = PlayerInput.getInput();
     int i = getMoveCount();
 
     while (i > 0) {
-      if (input.equals("right") && currentCol != GameBoard.getBoardWidth()) {
+      if (input == 2 && currentCol != board.getBoardWidth()) {
         currentCol += 1;
-      } else if (input.equals("left") && currentCol != 0) {
+        System.out.println("You took one step right.");
+      } else if (input == 4 && currentCol != 0) {
         currentCol -= 1;
-      } else if (input.equals("up") && currentRow != 0) {
+        System.out.println("You took one step left.");
+      } else if (input == 1 && currentRow != 0) {
         currentRow -= 1;
-      } else if (input.equals("down") && currentRow != GameBoard.getBoardHeight()) {
+        System.out.println("You took one step up.");
+      } else if (input == 3 && currentRow != board.getBoardHeight()) {
         currentRow += 1;
+        System.out.println("You took one step down.");
 
-      } else if ((input.equals("right") && currentCol == GameBoard.getBoardWidth())
-              || (input.equals("left") && currentCol == 0)
-              || (input.equals("up") && currentRow == 0)
-              || (input.equals("down") && currentRow == GameBoard.getBoardHeight())) {
+      } else
         throw new IllegalArgumentException("You hit a wall, try other direction. Moves left: " + getMoveCount());
       }
       i -= 1;
       setMoveCount(i);
-      GameBoard.setPlayerPosition(int a, b);
+      board.setPlayerPosition(...); //
 
-      if (GameBoard.getPlayerPosition() == GameBoard.getDoorPosition()) {
+      if (board.getPlayerPosition() == board.getDoorPosition()) {
         System.out.println("Congratulations! you found the DOOR!");
       }
       else {
         System.out.println("Keep moving. You have " + getMoveCount() + " moves left.");
       }
     }
-  }
 
 
     //getters and setters
