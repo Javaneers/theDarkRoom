ed08964e26589bdd460a4085db39ab14445d0841
mayamarks <maya.marks.dc@gmail.com>
2020-06-24 14:21:39 -0700

    fixed 0 input error

--- com.javaneers/src/com/javaneers/game/PlayerSprite.java
+++ com.javaneers/src/com/javaneers/game/PlayerSprite.java
@@ -24,8 +24,8 @@ public class PlayerSprite {
 
     while (i > 0) {
 
-       currentRow = board.getPlayerPosition("row");
-       currentCol = board.getPlayerPosition("col");
+      currentRow = board.getPlayerPosition("row");
+      currentCol = board.getPlayerPosition("col");
 
       System.out.println("----------------------------------------------------------");
       // Prompt player for move selection
@@ -39,34 +39,35 @@ public class PlayerSprite {
       //System.out.println("To exit the game press 9");
 
       input = ReadPlayerInput.getInput();
-
+      System.out.println(input);
       // player choice is '1' --> UP
       if (input == 1 && currentRow != 0) {
         currentRow -= 1;
         System.out.println("You took one step up.");
 
-      // player choice is '2' --> RIGHT
+        // player choice is '2' --> RIGHT
       } else if (input == 2 && currentCol != board.getBoardWidth() - 1) {
         currentCol += 1;
         System.out.println("You took one step right.");
 
-      // player choice is '3' --> DOWN
+        // player choice is '3' --> DOWN
       } else if (input == 3 && currentRow != board.getBoardHeight() - 1) {
         currentRow += 1;
         System.out.println("You took one step down.");
 
-      // player choice is '4' --> LEFT
+        // player choice is '4' --> LEFT
       } else if (input == 4 && currentCol != 0) {
         currentCol -= 1;
         System.out.println("You took one step left.");
 
-      // player choice is '0' -->  EXIT the GAME
-      } else if (input == 0 ) {
+        //player choice is '0' --> EXIT the GAME
+      } else if (input == 0) {
         i = 1;
-
-
-      } else
+        System.out.println("test input 0");
+      }
+      else {
         System.out.println("Oops!  You slam headfirst into the wall. Guess you can't go that way...");
+      }
       i -= 1;
       setMoveCount(i);
       board.updateBoard("player", currentRow, currentCol);
--- com.javaneers/src/com/javaneers/game/ReadPlayerInput.java
+++ com.javaneers/src/com/javaneers/game/ReadPlayerInput.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 public class ReadPlayerInput {
   //Restrict and reserve inputs for four cardinal directions and exiting the game
   private static final ArrayList<Integer> allowableInputs = new ArrayList<>(List.of(1,2,3,4,0));
-
+  static int result = 0;
 
 
   public static int getInput(){
     //Forced to initialize result variable because compiler didn't think recursion would resolve null values
-    int result = 0;
+    //int result = 0;
     Scanner inputMonitor = new Scanner(System.in);
     System.out.println("Type 1-4 into console then press enter to move your player");
 
