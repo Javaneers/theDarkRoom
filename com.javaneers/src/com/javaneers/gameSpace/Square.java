9359ba5e32ad97e59115407fcf20e3ea5223850e
itsOrD <matthew.beckerleg@gmail.com>
2020-06-24 15:57:33 -0700

    square class defined with setters/getters and ctor capable of toggling doorHere, playerHere, and boonHere staus

--- com.javaneers/src/com/javaneers/gameSpace/Square.java
+++ com.javaneers/src/com/javaneers/gameSpace/Square.java
@@ -1,5 +1,53 @@
 package com.javaneers.gameSpace;
 
-public class Square {
+public abstract class Square {
 
+  // PROPERTIES
+  public boolean doorHere;
+  public boolean playerHere;
+  public boolean boonHere;
+
+  // CTORs
+  public Square() {
+    // no-arg
+  }
+  public Square(String token) {
+    this();
+    try {
+      if ("door".equals(token)) {
+        setDoorHere(true);
+      }
+      else if ("player".equals(token)) {
+        setPlayerHere(true);
+      }
+      else if ("boon".equals(token)) {
+        setBoonHere(true);
+      }
+    } catch (IllegalArgumentException e) {
+      System.out.println("Argument token '" + token + "' is unacceptable.\n" +
+        "  The only special tokens allowed are: 'door', 'player', and 'boon'.");
+    }
+  }
+
+  // ACCESSORS
+  public boolean isDoorHere() {
+    return doorHere;
+  }
+  public void setDoorHere(boolean doorHere) {
+    this.doorHere = doorHere;
+  }
+
+  public boolean isPlayerHere() {
+    return playerHere;
+  }
+  public void setPlayerHere(boolean playerHere) {
+    this.playerHere = playerHere;
+  }
+
+  public boolean isBoonHere() {
+    return boonHere;
+  }
+  public void setBoonHere(boolean boonHere) {
+    this.boonHere = boonHere;
+  }
 }
